8d7c70bd608f3d0b54541ef68e8498657892a93c
noorhabib962 <engr.noorhabib962@gmail.com>
2025-02-24 20:19:10 +0500

    Initial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 3232
+let email = "engr.noorhabib962gmail.com"
+var password = "abc@323"
+var city = "Peshawar"
+var avg = 3.4
+x = 4
+// console.table([{account:accountId},{email:email},{password:password},{city:city}])
+
+
+console.log(typeof x)
+
+
+
